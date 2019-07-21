decfbbab29491d7979e6ba8f6a4029d250321282
Luca Rossitto <luca.rossitto@eng.it>
2019-07-21 16:06:24 +0200

    Menu hamburger con direttiva [ngbCollapse]

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,65 +1,59 @@
-import { AuthService } from './../services/auth.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../classes/user';
+import { AuthService } from './../services/auth.service';
 
 @Component({
-    selector: 'app-nav',
-    templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.css']
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  isCollapsed = false;
+  @Output() onNewUser = new EventEmitter();
+  isUserLoggedIn = false;
+  private username: string;
 
-    @Output() onNewUser = new EventEmitter();
-    isUserLoggedIn = false;
-    private username: string;
+  constructor(private auth: AuthService, private router: Router) {
+    auth.usersignedin.subscribe((user: User) => {
+      this.username = user.name;
+      this.isUserLoggedIn = true;
+    });
+    auth.userlogout.subscribe(() => {
+      this.username = '';
+      this.isUserLoggedIn = false;
+    });
+    auth.usersignedup.subscribe((user: User) => {
+      this.username = user.name;
+      this.isUserLoggedIn = true;
+    });
+  }
 
-    constructor(private auth: AuthService, private router: Router) {
-        auth.usersignedin.subscribe(
-            (user: User) => {
-                this.username = user.name;
-                this.isUserLoggedIn = true;
-            }
-        );
-        auth.userlogout.subscribe(
-            () => {
-                this.username = '';
-                this.isUserLoggedIn = false;
-            }
-        );
-        auth.usersignedup.subscribe(
-            (user: User) => {
-                this.username = user.name;
-                this.isUserLoggedIn = true;
-            }
-        );
+  ngOnInit() {
+    this.isUserLoggedIn = this.auth.isUserLoggedIn();
+    if (this.isUserLoggedIn) {
+      const user = this.auth.getUser();
+      this.username = user.name;
     }
+  }
 
-    ngOnInit() {
-        this.isUserLoggedIn = this.auth.isUserLoggedIn();
-        if(this.isUserLoggedIn) {
-            const user = this.auth.getUser();
-            this.username = user.name;
-        }
-    }
+  newUser() {
+    this.onNewUser.emit();
+  }
 
-    newUser() {
-        this.onNewUser.emit();
-    }
+  logout(e) {
+    e.preventDefault();
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
 
-    logout(e) {
-        e.preventDefault();
-        this.auth.logout();
-        this.router.navigate(['login']);
-    }
+  signIn(e) {
+    e.preventDefault();
+    this.router.navigate(['login']);
+  }
 
-    signIn(e) {
-        e.preventDefault();
-        this.router.navigate(['login']);
-    }
-
-    signUp(e) {
-        e.preventDefault();
-        this.router.navigate(['signup']);
-    }
+  signUp(e) {
+    e.preventDefault();
+    this.router.navigate(['signup']);
+  }
 }
